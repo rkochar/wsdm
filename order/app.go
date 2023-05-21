af0272ff573abfc83917244a6411287b2b4d3d83
DeepaliP98 <deepali846@gmail.com>
2023-05-21 23:10:19 +0200

    switch to local temporarily

--- order/app.go
+++ order/app.go
@@ -37,6 +37,7 @@ var ordersCollection *mongo.Collection
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+	fmt.Printf("Helleuuu")
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
