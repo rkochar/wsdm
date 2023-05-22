f5856390d8cf961ada5ee11d559b9269c5ca95f7
Rahul Kochar <rkochar9@gmail.com>
2023-05-22 22:26:07 +0200

    change imagepullpolicy and fix bug in kafka script

--- k8s/delete-microservice.sh
+++ k8s/delete-microservice.sh
@@ -14,4 +14,7 @@ kubectl delete -f ./k8s/ingress-service.yaml
 
 echo "Deleting lockmaster"
 kubectl delete -f ./k8s/lockmaster-sql-deployment.yaml
+
+echo "Release SQL pv"
 kubectl delete pvc mysql-pvc
+kubectl get pv | tail -n+2 | awk '$5 == "Released" {print $1}' | xargs -I{} kubectl patch pv {} --type='merge' -p '{"spec":{"claimRef": null}}'
