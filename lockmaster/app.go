e3932fbdbca146d32c3051749ca07d60aa980273
deepa <deepal@gmail.com>
2023-05-26 00:08:58 +0200

    messed up

--- lockmaster/app.go
+++ lockmaster/app.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql" // Import MySQL driver
+)
 
 func main() {
-    fmt.Println("Hello, Lockmaster!")
+	// Open a connection to the MySQL database
+	db, err := sql.Open("mysql", "your_username:your_password@tcp(mysql-service:3306)/your_database_name")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	// Ping the database to ensure the connection is established
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Execute a query
+	rows, err := db.Query("SELECT * FROM your_database_name")
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Printf("I did it")
+	}
+	defer rows.Close()
+
+	// Iterate through the result set
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println("ID:", id, "Name:", name)
+	}
+
+	// Insert data into the database
+	_, err = db.Exec("INSERT INTO users (name) VALUES (?)", "John Doe")
+	if err != nil {
+		panic(err.Error())
+	}
 }
