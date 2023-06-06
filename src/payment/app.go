242ed7d0eae53ff6af2209a6dc9394b4dc6d41e4
DeepaliP98 <deepali846@gmail.com>
2023-06-06 18:52:04 +0200

    Db fix name

--- src/payment/app.go
+++ src/payment/app.go
@@ -107,7 +107,7 @@ func main() {
 
 func setupDBConnections(ctx context.Context) error {
 	for i := 0; i < shared.NUM_DBS; i++ {
-		mongoURL := fmt.Sprintf("mongodb://orderdb-service-%d:27017", i)
+		mongoURL := fmt.Sprintf("mongodb://paymentdb-service-%d:27017", i)
 		fmt.Printf("%d MongoDB URL: %s\n", i, mongoURL)
 		var err error
 		var client *mongo.Client
